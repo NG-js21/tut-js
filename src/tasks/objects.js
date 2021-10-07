ab619851ea0d21416a140c24ddeec6f451feec26
NG-js21 <92050085+NG-js21@users.noreply.github.com>
2021-10-07 12:53:53 +0200

    solved_objects

--- src/tasks/objects.js
+++ src/tasks/objects.js
@@ -3,8 +3,13 @@ const objectsTask = {
     //вернуть новый объект, что-бы в нём ключи и значения были поменяны местами
     //swapKeysWithValues({a: 'aa', b: 'bb'}) => { aa: 'a', bb: 'b' };
     swapKeysWithValues(obj) {
+        let inverted = new Object();
 
+        for (let key in obj) {
+            inverted[obj[key]] = key;
+        }
+
+        return inverted
     },
 
 };
-
