34ea746b5ceb32fef242c28a7df2ac7c8179e027
ntlgracheva <ntlgracheva@gmail.com>
2021-10-11 11:38:29 +0200

    updated_basic

--- src/tasks/basics.js
+++ src/tasks/basics.js
@@ -3,7 +3,7 @@ const basicsTask = {
      * на вход ф-ция получает 3 числа, нужно вернуть сумму этих чисел
      */
     sum(a, b, c) {
-    return a + b + c;
+        return a + b + c;
     },
     /**
      * аргументами могут быть не только числа
@@ -11,12 +11,12 @@ const basicsTask = {
      * 
      */
     safeSum(a, b, c) {
-    let sum = 0;
+        let sum = 0;
 
-    for (let i = 0; i < arguments.length; i++) {
-        if (typeof (arguments[i]) == "number") { sum += arguments[i] }
-    }
-    return sum
+        for (let i = 0; i < arguments.length; i++) {
+            if (typeof (arguments[i]) == "number") { sum += arguments[i] }
+        }
+        return sum
     },
 
     /**
@@ -24,9 +24,11 @@ const basicsTask = {
      * нужно найти максимальное
      */
     max(a, b, c) {
-    let maxNum = a;
-    for (let i = 0; i < arguments.length; i++) { maxNum > arguments[i] ? maxNum = maxNum : maxNum = arguments[i] }
-    return maxNum;
+        let maxNum = arguments[0];
+        for (let i = 1; i < arguments.length; i++) {
+            maxNum = maxNum > arguments[i] ? maxNum : arguments[i];
+        }
+        return maxNum;
     },
     /**
      * 
@@ -34,27 +36,33 @@ const basicsTask = {
      * нужно найти минимальное
      */
     min(a, b, c) {
-    let minNum = a;
-    for (let i = 0; i < arguments.length; i++) { minNum < arguments[i] ? minNum = minNum : minNum = arguments[i] }
-    return minNum;
+        let minNum = a;
+        for (let i = 0; i < arguments.length; i++) {
+            minNum < arguments[i] ? minNum = minNum : minNum = arguments[i];
+        }
+        return minNum;
     },
     //если n целое число то вернуть true, иначе false
     isIntegerNumber(n) {
-    return Number.isInteger(n);
+        return n - Math.floor(n) === 0;
     },
     //ф-ция должна уметь округлить число n до l знаков после запятой, 1 <= l <= 10
     //roundNumber(0.66666666, 2) => 0.67
     //roundNumber(1, 1) => 1.0
     roundNumber(n, l) {
-    return n.toPrecision(l)
+        let string = String(n);
+        let symbolIndex = string.indexOf(".");
+
+        return +string.slice(0, symbolIndex + 1 + l);
     },
     //на входе массив чисел
     //ф-ция должна вернуть массив процентовб т.е. сколько процентов занимает каждое число от общей суммы
     //percentage([1, 1]) => [0.5, 0.5]
     //percentage([1, 2, 3]) => [0.17, 0.33, 0.6]
     percentage(numbers) {
-    let sum = numbers.reduce((acc, num) => acc + num, 0)
+        let sum = numbers.reduce((acc, num) => acc + num, 0)
 
-    return numbers.map(num => (num / sum).toPrecision(2))
+        return numbers.map(num => (num / sum).toPrecision(2))
     },
 };
+console.log(basicsTask.sum(1, 2, 3));
